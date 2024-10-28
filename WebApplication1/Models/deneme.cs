0925fae2a34af54f673d480f7b4920e339dd8605
seheroguz <20210808004@ogr.akdeniz.edu.tr>
2024-10-28 13:11:58 +0300

    add Idd

--- WebApplication1/Models/deneme.cs
+++ WebApplication1/Models/deneme.cs
@@ -4,6 +4,6 @@
     {
         public int id { get; set; }
         public string name { get; set; } = string.Empty;
-        public int orderId { get; set; }
+        public int Idd { get; set; }
     }
 }
